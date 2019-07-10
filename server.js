71b065c533ea991175321bc8961502915e81a078
Ian Hutomo <ianhutomo@gmail.com>
2019-07-10 15:48:54 +0800

    Add Additional dependency files, Add crypto ticker function

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
+// Hello World & testing page
 app.get("/", function(req, res){
   res.send("<h1>Hello World!</h1>");
 });
@@ -14,8 +16,10 @@ app.get("/about", function(req,res){
   res.send("<h1>My Bio</h1>");
 });
 
+//BMI Calculation
+/*
 app.get("/BMI", function(req,res){
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/BMI.html");
 });
 
 app.post("/BMI", function(req, res){
@@ -29,6 +33,36 @@ app.post("/BMI", function(req, res){
 
 });
 
+*/
+
+//ticker site
+
+app.get("/ticker", function(req, res){
+  res.sendFile(__dirname + "/ticker.html");
+});
+
+app.post("/", function(req,res){
+
+  var crypto = req.body.crypto;
+  var fiat = req.body.fiat;
+  var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+  var finalURL = baseURL + crypto + fiat;
+
+  console.log(finalURL);
+
+  request(finalURL, function(error, response, body){
+
+  var data = JSON.parse(body);
+  var price = data.last;
+
+  res.write("The current price of " + crypto + " is " + price +  fiat);
+  res.send();
+  });
+
+});
+
+
+//starting server to listen to request
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
